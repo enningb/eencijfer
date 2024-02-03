9aae039efb56426ea48aa2f2d0f529710040a5fc
Bram Enning <bramenning@gmail.com>
2024-02-03 12:16:06 +0100

    added docstrings

--- eencijfer/column_converters.py
+++ eencijfer/column_converters.py
@@ -1,3 +1,4 @@
+"""Converters for columns on read-time."""
 import numpy as np
 import pandas as pd
 
@@ -6,34 +7,79 @@ from eencijfer import column_converter
 
 @column_converter
 def convert_to_object(x: pd.Series) -> pd.Series:
+    """Convert column to string.
+
+    Args:
+        x (pd.Series): column
+
+    Returns:
+        pd.Series: all values will be string
+    """
     return str(x)
 
 
 @column_converter
 def convert_to_int64(x):
+    """Convert column to int64.
+
+    Args:
+        x (pd.Series): column
+
+    Returns:
+        pd.Series: all values will be int64
+    """
     return int(x)
 
 
 @column_converter
 def convert_to_float64(x):
+    """Convert column to float64.
+
+    Args:
+        x (pd.Series): column
+
+    Returns:
+        pd.Series: all values will be float64
+    """
     return float(x)
 
 
 @column_converter
 def convert_to_date(x):
-    """Zet x om naar een datumveld"""
+    """Convert column to date.
+
+    Args:
+        x (pd.Series): column
+
+    Returns:
+        pd.Series: all values will be dates.
+    """
     return pd.to_datetime(x, format="%Y%m%d")
 
 
 @column_converter
 def convert_to_none(x):
-    """Zet x om naar een leeg veld"""
+    """Convert column to None.
+
+    Args:
+        x (pd.Series): column
+
+    Returns:
+        pd.Series: all values will be None
+    """
     return None
 
 
 @column_converter
 def convert_geslacht(x):
-    """Zet x om naar een leesbare geslachtsomschrijving"""
+    """Convert column to readable geslacht (man, vrouw)
+
+    Args:
+        x (pd.Series): column
+
+    Returns:
+        pd.Series: all values will be man or vrouw
+    """
     if x == "M":
         return "man"
     elif x == "V":
@@ -44,9 +90,13 @@ def convert_geslacht(x):
 
 @column_converter
 def convert_opleidingsvorm(x):
-    """Converts a value to int and than adda Opleidingsvorm as string.
-    :param x:
-    :return: a string
+    """Converts a value to int and than adds opleidingsvorm as string.
+
+    Args:
+        x (pd.Series): column
+
+    Returns:
+        pd.Series: all values will be in voltijd, deeltijd, duaal.
     """
 
     if int(x) == 1:
@@ -61,7 +111,14 @@ def convert_opleidingsvorm(x):
 
 @column_converter
 def convert_to_int_zero_to_nan(x):
-    """converts a 0 to NaN"""
+    """Converts all values to int, except 0. These will be Nan.
+
+    Args:
+        x (pd.Series): column
+
+    Returns:
+        pd.Series: all values will int or Nan.
+    """
     if int(x) == 0:
         return np.nan
     else:
