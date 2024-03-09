38c2c4939a17de53a98dc36a8820d11fe2e1a043
Bram Enning <bramenning@gmail.com>
2024-03-09 13:00:35 +0100

    added code for adding local_id (studentnummer)

---
+++ eencijfer/assets/transformations/local_data.py
@@ -0,0 +1,37 @@
+"""Add local, institution specific, data."""
+
+import logging
+
+import pandas as pd
+
+from eencijfer.settings import config
+
+logger = logging.getLogger(__name__)
+
+
+def _add_local_id(data: pd.DataFrame) -> pd.DataFrame:
+    """Adds a local id (e.g. studentnummer).
+
+    Args:
+        data (pd.DataFrame): df with PersoonsgebondenNummer.
+
+    Raises:
+        Exception: Merge error.
+
+    Returns:
+        pd.DataFrame: data with local-id added.
+    """
+
+    logger.info('Adding local_ids.')
+    fpath = config.getpath('local_id', 'fpath')
+    left_on = config.get('local_id', 'left_on')
+    right_on = config.get('local_id', 'right_on')
+    how = config.get('local_id', 'how')
+    logger.info(f'... merging {left_on} with {right_on} for local_ids.')
+
+    local_ids = pd.read_parquet(fpath)
+    result = pd.merge(data, local_ids, left_on=left_on, right_on=right_on, how=how)
+    if (how == 'left') and (len(data) != len(result)):
+        raise Exception('Merge went wrong!')
+
+    return result
