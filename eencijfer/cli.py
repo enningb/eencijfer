9c5b215ee1c66f4de764a9b36bf30b4af603703d
Bram Enning <bramenning@gmail.com>
2024-03-12 20:04:05 +0100

    add: create result dir if it does not exist

--- eencijfer/cli.py
+++ eencijfer/cli.py
@@ -71,6 +71,10 @@ def convert(
 ):
     """Convert eencijfer-files to desired exportformat, with or without PII."""
 
+    result_dir = config.getpath('default', 'result_dir')
+    if not result_dir.is_dir():
+        Path(result_dir).mkdir(parents=True, exist_ok=True)
+
     _convert_to_export_format(export_format=export_format.value, use_column_converters=use_column_converters)
 
     _replace_all_pgn_with_pseudo_id_remove_pii_local_id(
